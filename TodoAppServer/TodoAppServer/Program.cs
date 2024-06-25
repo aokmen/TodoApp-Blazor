54106584f8511f205ecd8c55ac8ef9872062e7bd
Abdullah <129761775+aokmen@users.noreply.github.com>
2024-06-25 23:52:20 +0200

    Update Program.cs

--- TodoAppServer/TodoAppServer/Program.cs
+++ TodoAppServer/TodoAppServer/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TodoAppServer.Components;
+using TodoAppServer.Components; //TodoAppServer
 
 var builder = WebApplication.CreateBuilder(args);
 
